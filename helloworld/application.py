e16b7ec0cf337b6d3866aa2efafd4c54e6523aad
Yoavbl <97474272+Yoavbl@users.noreply.github.com>
2023-08-10 20:01:43 +0500

    code completed

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,121 @@
 #!flask/bin/python
-import json
-from flask import Flask, Response
+from flask import Flask, request, redirect, render_template, session
+from helloworld.utils import (
+    save_user_data,
+    upload_picture,
+    get_user,
+    get_image_list,
+    compare_faces,
+    get_file_url,
+    get_user_by_image,
+    filter_data,
+)
+import os
 from helloworld.flaskrun import flaskrun
 
-application = Flask(__name__)
 
-@application.route('/', methods=['GET'])
-def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+app = Flask(__name__)
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+app.config["SECRET_KEY"] = "221714a843f5b44f8b9e71cf77b87ba7d2020140d4641df9"
 
-if __name__ == '__main__':
-    flaskrun(application)
+
+# Route for the login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if session.get("user"):
+        return redirect("/")
+
+    if request.method == "POST":
+        email = request.form["email"]
+
+        user_info = get_user(email)
+
+        if user_info:
+            session["user"] = user_info
+            return redirect("/")
+
+        else:
+            return redirect("/signup")
+
+    return render_template("login.html")
+
+
+# Route for the signup page
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        file = request.files["profile_picture"]
+        data = {**request.form}
+
+        data["profile_picture"] = upload_picture(file)
+        save_user_data(data)
+        return redirect("/login")
+
+    return render_template("signup.html")
+
+
+@app.route("/find_by_picture", methods=["POST", "GET"])
+def find_by_picture():
+    if not session.get("user"):
+        return redirect("/login")
+    result = []
+
+    if request.method == "POST":
+        file = request.files["file"]
+        # Save the uploaded file temporarily
+        uploaded_file_path = os.path.join("uploads", file.filename)
+        file.save(uploaded_file_path)
+
+        target_images = get_image_list()
+        users_images_url = []
+
+        # Compare faces with each image
+        for target_image in target_images:
+            face_matches = compare_faces(uploaded_file_path, target_image)
+            if face_matches:
+                users_images_url.append(
+                    get_file_url(target_image),
+                )
+
+        os.remove(uploaded_file_path)
+
+        result = []
+        for url in users_images_url:
+            user_info = get_user_by_image(url)
+            if user_info:
+                result.append(user_info)
+
+    return render_template("result.html", result=result)
+
+
+@app.route("/find_by_details", methods=["POST", "GET"])
+def find_by_details():
+    if not session.get("user"):
+        return redirect("/login")
+
+    result = []
+
+    if request.method == "POST":
+        data = request.form.to_dict()
+
+        data = {
+            key: value
+            for key, value in data.items()
+            if value is not None and value != ""
+        }
+
+        result = filter_data(data)
+
+    return render_template("result.html", result=result)
+
+
+@app.route("/")
+def index():
+    if not session.get("user"):
+        return redirect("/login")
+    user_info = session["user"]
+    return render_template("index.html", user=user_info)
+
+
+if __name__ == "__main__":
+    flaskrun(app)
---
+++ helloworld/utils.py
@@ -0,0 +1,125 @@
+import boto3
+
+TABLE_NAME = "users"
+AWS_REGION = "eu-north-1"
+BUCKET_NAME = "usersmediabucket"
+
+dynamodb = boto3.client("dynamodb", region_name=AWS_REGION)
+s3_client = boto3.client("s3", region_name="us-west-2")
+
+rekognition_client = boto3.client("rekognition", region_name="us-west-2")
+
+
+def upload_picture(file):
+    filename = file.filename
+    s3_client.upload_fileobj(file, BUCKET_NAME, filename)
+    profile_picture = f"https://{BUCKET_NAME}.s3.amazonaws.com/{filename}"
+    return profile_picture
+
+
+def save_user_data(data):
+    dynamodb.put_item(
+        TableName=TABLE_NAME,
+        Item={
+            "first_name": {"S": data["first_name"]},
+            "last_name": {"S": data["last_name"]},
+            "email": {"S": data["email"]},
+            "phone_number": {"S": data["phone_number"]},
+            "height": {"S": data["height"]},
+            "age": {"S": str(data["age"])},
+            "gender": {"S": data["gender"]},
+            "hobby": {"S": data["hobby"]},
+            "education": {"S": data["education"]},
+            "designation": {"S": data["designation"]},
+            "profile_picture": {"S": data["profile_picture"]},
+            "bio": {"S": data["bio"]},
+        },
+    )
+    return True
+
+
+def get_user(email):
+    data = dynamodb.get_item(TableName=TABLE_NAME, Key={"email": {"S": email}})
+    data = data.get("Item")
+    if data:
+        return convert_to_dict(data)
+    return None
+
+
+def get_user_by_image(url):
+    data = dynamodb.scan(
+        TableName=TABLE_NAME,
+        FilterExpression="profile_picture = :val",
+        ExpressionAttributeValues={":val": {"S": url}},
+    )
+    data = data.get("Items")
+    if data:
+        user_info = convert_to_dict(data[0])
+        return user_info
+    return None
+
+
+def get_image_list():
+    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
+    target_images = [obj["Key"] for obj in response.get("Contents", [])]
+    return target_images
+
+
+def compare_faces(source_image_path, target_image):
+    with open(source_image_path, "rb") as local_source_image_file:
+        response = rekognition_client.compare_faces(
+            SourceImage={"Bytes": local_source_image_file.read()},
+            TargetImage={"S3Object": {"Bucket": BUCKET_NAME, "Name": target_image}},
+            SimilarityThreshold=80,
+        )
+
+        return response.get("FaceMatches", [])
+
+
+# Function to generate S3 object URL
+def get_file_url(key):
+    url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{key}"
+    return url
+
+
+def filter_data(data):
+    filter_expression_parts = []
+    expression_attribute_values = {}
+
+    for column_name, value in data.items():
+        expression_key = f":{column_name}"
+        filter_expression_parts.append(f"{column_name} = {expression_key}")
+        expression_attribute_values[expression_key] = {"S": value}
+
+    filter_expression = " AND ".join(filter_expression_parts)
+
+    response = dynamodb.scan(
+        TableName=TABLE_NAME,
+        FilterExpression=filter_expression,
+        ExpressionAttributeValues=expression_attribute_values,
+    )
+
+    items = response.get("Items")
+    if items:
+        items = [convert_to_dict(item) for item in items]
+        return items
+    else:
+        return []
+
+
+def convert_to_dict(data):
+    user_info = {
+        "first_name": data["first_name"]["S"],
+        "last_name": data["last_name"]["S"],
+        "email": data["email"]["S"],
+        "phone_number": data["phone_number"]["S"],
+        "height": data["height"]["S"],
+        "age": data["age"]["S"],
+        "gender": data["gender"]["S"],
+        "hobby": data["hobby"]["S"],
+        "education": data["education"]["S"],
+        "designation": data["designation"]["S"],
+        "profile_picture": data["profile_picture"]["S"],
+        "bio": data["bio"]["S"],
+    }
+    return user_info
